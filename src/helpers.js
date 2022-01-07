12018b5f73b92c1aae292a7f41657ddae8513dda
agolautner <lautner.agoston@gmail.com>
2022-01-07 10:32:52 +0100

    finished assignment with a bit of help

--- src/helpers.js
+++ src/helpers.js
@@ -4,4 +4,18 @@
 
 //         choice(items): returns a randomly selected item from array of items
 //         remove(items, item): removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
+function choice(items) {
+    let idx = Math.floor(Math.random() * items.length);
+    return items[idx];
+  }
 
+function remove(item, items) {
+    let itemIsIn = (items.indexOf(item) !== -1);
+
+    return(
+        itemIsIn ? items.splice(items.indexOf(item), 1) : undefined
+    )
+    
+}
+
+export {choice, remove}
--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@
 //     Remove the fruit from the array of fruits
 //     Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
 
+import fruits from "./food";
+import { choice, remove } from "./helpers";
+
+let randomFruit = choice(fruits);
+console.log(`I'd like one ${randomFruit}, please!`);
+console.log(`Here you go: ${randomFruit}`);
+console.log("Delicious! May I have another?");
+remove(randomFruit, fruits);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} other kinds left.`);
